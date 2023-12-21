0b71c3acd2d507ef93eb4d1bcacaec371afbe37d
nguyenhuuton123 <nhtonx1@gmail.com>
2023-12-21 10:10:27 +0700

    abc

---
+++ demoJSP-Servlet/src/main/java/com/entity/CartLine.java
@@ -0,0 +1,14 @@
+package com.entity;
+
+public class CartLine {
+
+    private int topping_ids;
+
+    private int size_id;
+
+    private int product_id;
+
+    private int quantity;
+
+    private  int cart_id;
+}
